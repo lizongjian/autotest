625300464629b8e53c9e5c78b4c2858d5e4e9ae7
lizongjian <546660862@qq.com>
2020-05-04 09:11:37 +0800

    测试

--- src/main/java/com/autotest/App.java
+++ src/main/java/com/autotest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊Hello World!啊" );
     }
 }
