13f70a3adaf959a1b0d03d1c75e973d77a060b21
lizongjian <546660862@qq.com>
2020-05-16 13:52:28 +0800

    log4j 1.2.17 bug

--- src/main/java/com/autotest/App.java
+++ src/main/java/com/autotest/App.java
@@ -1,13 +1,20 @@
 package com.autotest;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊Hello World!啊" );
-    }
+import org.apache.log4j.Logger;
+import org.apache.log4j.net.SimpleSocketServer;
+
+public class App {
+
+    private static final Logger log = Logger.getLogger(SimpleSocketServer.class);
+ 
+    public static void main(String[] args) throws Exception {
+ 
+        System.out.println("start log4j Socket Server  Port 5000");
+ 
+        String[] argss = { "5000", "src/log4j.properties" };
+ 
+        SimpleSocketServer.main(argss);
+ 
+        log.info("succ");
+    }    
 }
